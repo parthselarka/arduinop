b076138ec0e9193983cb722389c44383245c4af9
parthselarka <116080750+parthselarka@users.noreply.github.com>
2024-11-22 10:46:14 +0530

    Add files via upload

--- ledtest.ino
+++ ledtest.ino
@@ -1,17 +1,22 @@
-// Define the pin for the LED
-const int ledPin = 13; // Use 13 for onboard LED, or change for an external LED
-
-void setup() {
-  // Initialize the LED pin as an output
-  pinMode(ledPin, OUTPUT);
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
 }
 
-void loop() {
-  // Turn the LED on
-  digitalWrite(ledPin, HIGH);
-  delay(1000); // Wait for 1 second
-
-  // Turn the LED off
-  digitalWrite(ledPin, LOW);
-  delay(1000); // Wait for 1 second
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(250);
+  digitalWrite(13, LOW);
+  delay(50);
+  digitalWrite(12, HIGH);
+  delay(250);
+  digitalWrite(12, LOW);
+  delay(50);
+  digitalWrite(11, HIGH);
+  delay(250);
+  digitalWrite(11, LOW);
+  delay(50);
 }
